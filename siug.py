89c1e554056830812dbf06d6634b23d543e634dc
Fernando <fernandonarea6@gmail.com>
2025-12-27 14:45:51 -0500

    formatting

--- siug.py
+++ siug.py
@@ -19,7 +19,7 @@ try:
     i_dia = driver.find_element(By.NAME, "ctl00$MainContent$LoginUser$TXT_DIA")
     i_mes = driver.find_element(By.NAME, "ctl00$MainContent$LoginUser$TXT_MES")
     btn_login = driver.find_element(By.NAME, "ctl00$MainContent$LoginUser$LoginButton")
-    
+
     # Asignacion de valores
     i_usuario.send_keys("1311893711")
     i_password.send_keys("Fer27112002")
@@ -28,28 +28,32 @@ try:
     btn_login.click()
 
     time.sleep(3)
-    
+
     btn_close = driver.find_element(By.CLASS_NAME, "close")
     btn_close.click()
-    
+
     btn_horario = driver.find_element(By.NAME, "ctl00$MainContent$BTN_CONSULTA_HORARIO")
     btn_horario.click()
 
     time.sleep(3)
-    select_carrera = Select(driver.find_element(By.NAME, "ctl00$MainContent$DDL_CARRERA"))
+    select_carrera = Select(
+        driver.find_element(By.NAME, "ctl00$MainContent$DDL_CARRERA")
+    )
 
-    print('Entrando al select de carrera')
+    print("Entrando al select de carrera")
     select_carrera.select_by_value("0311  ")
-    
-    time.sleep(3)
-
-    select_periodo = Select(driver.find_element(By.NAME, "ctl00$MainContent$DDL_PLECTIVO"))
-    print('Seleccionando el periodo')
-    select_periodo.select_by_value('35')
 
     time.sleep(3)
 
-    btn_clases = driver.find_element(By.ID, 'MainContent_BTN_CLASES')
+    select_periodo = Select(
+        driver.find_element(By.NAME, "ctl00$MainContent$DDL_PLECTIVO")
+    )
+    print("Seleccionando el periodo")
+    select_periodo.select_by_value("35")
+
+    time.sleep(3)
+
+    btn_clases = driver.find_element(By.ID, "MainContent_BTN_CLASES")
     print("Haciendo click en el boton de 'Clases Virtuales'")
     btn_clases.click()
 
@@ -62,9 +66,9 @@ try:
 
     time.sleep(5)
 
-    btn_ingreso_clase = driver.find_element(By.ID, 'btnIrReunion')
+    btn_ingreso_clase = driver.find_element(By.ID, "btnIrReunion")
     btn_ingreso_clase.click()
-    
+
     time.sleep(5)
 
     alert = driver.switch_to.alert
@@ -75,4 +79,3 @@ try:
 
 except Exception as e:
     print(f"Ocurrió un error: {e}")
-
